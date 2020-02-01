c45b221e4c589c7634951fce4b35800dbff2fdc0
Section9-Togusa <corbindac@gmail.com>
2020-02-01 18:46:43 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
-## code your solution here. 
+class Cat 
+    attr_accessor :name 
+    def initiazlie 
+        @name = name 
+        @meow = meow 
+    end 
+
+    def meow
+        puts "meow!"
+    end 
+end 
+gf = Cat.new 
+gf.meow
